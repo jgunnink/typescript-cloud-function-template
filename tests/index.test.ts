94fee3e30cf9687eca5a7f8b3917f1ce96c44691
JK Gunnink <jgunnink@gmail.com>
2022-03-09 11:18:34 +0800

    naming variables

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,25 +4,25 @@ import { Request, Response } from "express";
 
 interface testObject {
   args: Partial<Request>;
-  response: any; // We use any here to allow any kind of test response from the function to be passed in.
+  expected: any; // We use any here to allow any kind of test response from the function to be passed in.
 }
 
 describe("when a message is provided", () => {
   const tests: testObject[] = [
-    { args: { body: { message: "Hi" } }, response: { message: "Hi" } },
-    { args: { body: { message: "12" } }, response: { message: "12" } },
+    { args: { body: { message: "Hi" } }, expected: { message: "Hi" } },
+    { args: { body: { message: "12" } }, expected: { message: "12" } },
   ];
 
-  tests.forEach(({ args, response }) => {
+  tests.forEach(({ args, expected }) => {
     it(`returns ${args.body.message} when request body is ${JSON.stringify(args.body)}`, () => {
       const result = helloWorld(args as Request, { send: () => null } as Response);
-      expect(result).to.deep.eq(response);
+      expect(result).to.deep.eq(expected);
     });
   });
 });
 
 describe("when a message is not provided", () => {
-  const tests = [
+  const tests: testObject[] = [
     { args: { body: { message: "" } }, expected: { message: "Hello World!" } },
     { args: { body: { message: undefined } }, expected: { message: "Hello World!" } },
     { args: { body: {} }, expected: { message: "Hello World!" } },
